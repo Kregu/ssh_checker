2d684f11fb6466f1805612b9e825f498cbeea0d4
Sergey Gorokhov <sergey.login@gmail.com>
2020-02-07 20:54:51 +0300

    Add method send to telegram

--- pssh.py
+++ pssh.py
@@ -1,4 +1,7 @@
 import paramiko
+import requests
+import settings
+
 from datetime import datetime
 from time import sleep
 
@@ -6,29 +9,48 @@ host = '192.168.1.1'
 user = 'admin'
 secret = '13425'
 port = 22
-commands = ['ls', 'show sys']
+commands = ['show sys']
 encoding = 'utf-8'
 
-while True:
+def send_telegram(text: str):
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, password=secret, port=port)
+    url = "https://api.telegram.org/bot" + settings.token
+    chat_id = settings.chat_id
 
-    data = bytes()
-    for command in commands:
-        stdin, stdout, stderr = client.exec_command(command)
-        data += stdout.read() + stderr.read()
-        sleep(0.1)
-             
-    client.close()
+    method = url + "/sendMessage"
 
-    
-    with open('data.txt', 'a') as file_data:
-        file_data.write(str(datetime.today().strftime("%Y-%m-%d %H:%M:%S") + '\n'))
-        file_data.write(data.decode(encoding) + '\n')
+    r = requests.post(method, data={
+         "chat_id": chat_id,
+         "text": text
+          })
+
+    if r.status_code != 200:
+        print('Error send to telegram')
+
+def main():
+    while True:
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=host, username=user, password=secret, port=port)
+
+        data = bytes()
+        for command in commands:
+            stdin, stdout, stderr = client.exec_command(command)
+            data += stdout.read() + stderr.read()
+            sleep(0.1)
+                 
+        client.close()
+
+        
+        with open('data.txt', 'a') as file_data:
+            file_data.write(str(datetime.today().strftime("%Y-%m-%d %H:%M:%S") + '\n'))
+            file_data.write(data.decode(encoding) + '\n')
 
 
-    sleep(5)
+        send_telegram(data.decode(encoding))
+        sleep(10)
 
+if __name__ == '__main__':
+    main()
 
